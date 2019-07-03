e7b0017612fca7cce3a17d0341a0611373c9f5c9
Nick <nikita.glu@icloud.com>
2019-07-03 22:19:20 +0300

    convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Никита on 03/07/2019.
+//  Copyright © 2019 Nikita Glushchenko. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+}
